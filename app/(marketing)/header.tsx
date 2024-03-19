1b0704ce2afbcd3b48dc60f2a3763e15cc8b508d
Aadarsh <aadarshjha6783@gmail.com>
2024-03-19 16:54:18 +0530

    Homepage initialisation

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button"
+import { ClerkLoaded, ClerkLoading, SignIn, SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { Loader, Loader2 } from "lucide-react"
+import Image from "next/image"
+
 interface HeaderProps {
 
 }
@@ -5,7 +10,30 @@ interface HeaderProps {
 export const Header = ({ }: HeaderProps) => {
     return (
         <header className="h-20 w-full border-b-2 border-gray-200">
-            Header
+            <div className="lg:max-w-screen-lg mx-auto flex items-center
+            justify-between h-full ">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src={'/svg/mascot.svg'} alt="Trilingo Main Logo" height={40} width={40} />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+                        Duolingo
+                    </h1>
+                </div>
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/"/>
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton mode="modal" afterSignUpUrl="/learn" afterSignInUrl="/learn">
+                            <Button variant={"ghost"}>
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
         </header>
     )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,51 @@
 import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignIn, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <p className="font-bold text-green-500 text-4xl">Hi theres</p>
+    <div className="mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center max-w-[988px] p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[425px] lg:h-[425px] mb-8 lg:mb-0">
+        <Image src={'/svg/hero.svg'} fill alt="Duolingo Here Pic" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice, and master languages on Duolingo.
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                <Button variant={"secondary"} size={"lg"} className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                <Button variant={"primaryOutline"} size={"lg"} className="w-full">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <Button
+                variant={"secondary"}
+                size={"lg"}
+                className="w-full"
+              >
+                <Link href={'/learn'}>
+                  Continue Learning
+                </Link>
+              </Button>
+            </SignedIn>
+
+          </ClerkLoaded>
+        </div>
       </div>
     </div>
   );
